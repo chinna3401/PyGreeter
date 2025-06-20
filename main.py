6b3a84d5a6d6f1c7eee0956d3bdda84b019a659a
Madhu Amarthaluru <chinna3401@gmail.com>
2025-06-20 15:18:27 -0500

    First Commit from VS

---
+++ main.py
@@ -0,0 +1,10 @@
+#simple python program to greet the user
+
+def greet_user():
+    name = input("What's your name?")
+    print(f"Hello, {name}! welcome to the program.")
+
+
+ # call the function
+
+greet_user()
